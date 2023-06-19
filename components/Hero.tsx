37c05ee44a0a15e4268e4a3f175b6f23a21b9779
ace malto <maltoace9@gmail.com>
2023-06-19 15:21:59 +0800

    initial

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className='hero'>
+            <div className='hero_image'>
+                <Image src='/avatar.png' 
+                    width={300}
+                    height={300}
+                    alt='avatar'
+                    className=' object-contain'
+                />
+                <div className='flex flex-col justify-center items-center'>
+                    <h1>
+                        - Ace Malto -
+                    </h1>
+                    <span>
+                        Full Stack Developer
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 
