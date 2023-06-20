4b393db1955e51efae7e0af53f38a5bb05bcf22d
ace malto <maltoace9@gmail.com>
2023-06-20 22:35:38 +0800

    initial

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,13 +11,18 @@ const Hero = () => {
                     alt='avatar'
                     className=' object-contain'
                 />
-                <div className='flex flex-col justify-center items-center'>
-                    <h1>
+                <div className='flex flex-col justify-center text-[#9EF0F0] items-center mt-5'>
+                    <h1 className='text-6xl -rotate-6'>
                         - Ace Malto -
                     </h1>
-                    <span>
+                    <span className='mt-10 text-lg font-bold'>
                         Full Stack Developer
                     </span>
+                    <div className='hero_links'>
+                        <div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
